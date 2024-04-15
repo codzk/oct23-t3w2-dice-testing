f22c1036220614ea72ca704c74de0d41f4c84129
codzk <zekemilne@gmail.com>
2024-04-16 03:38:38 +1000

    docs: add docs to dice roll functions

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,5 +1,13 @@
 
 
+/**
+ * Generate a random number between 1 and a given size.
+ *
+ * @param [diceSize=6] - Number representing the size of the dice you want.
+ * @returns A random number between 1 and the given size.
+ */
+
+
 function rollDice(diceSize = 6) {
     let rollResult = Math.floor(Math.random() * diceSize) + 1
 
@@ -21,6 +29,16 @@ function rollDiceWithAdvantage(diceSize = 6){
 
     return result;
 }
+
+
+
+/**
+ * Generate a random number between 1 and a given size twice, and identifying the highest number.
+ *
+ * @param [diceSize=6]
+ * @returns A random number between 1 and a given size twice, and identifying the smallest number.
+ */
+
 
 function rollDiceWithDisadvantage(diceSize = 6){
     let result = {
