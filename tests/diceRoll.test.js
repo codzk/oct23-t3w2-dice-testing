94a63266702b43c469ab98724ff428e17531cfe0
codzk <zekemilne@gmail.com>
2024-04-17 03:15:56 +1000

    test: refactored test of dice varieties

--- tests/diceRoll.test.js
+++ tests/diceRoll.test.js
@@ -48,45 +48,84 @@ describe('Casual gamer...', () => {
 });
 
 describe('As a dungeon master...', () => {
+
+    let diceVarieties = [
+        {
+            diceSize: 4,
+            diceName: "D4"
+        },
+        {
+            diceSize: 6,
+            diceName: "D6"
+        },
+        {
+            diceSize: 8,
+            diceName: "D8"
+        },
+        {
+            diceSize: 10,
+            diceName: "D10"
+        },
+        {
+            diceSize: 12,
+            diceName: "D12"
+        },
+        {
+            diceSize: 20,
+            diceName: "D20"
+        },
+        {
+            diceSize: 100,
+            diceName: "D100"
+        },
+        
+        
+    ]
+
+    test.each(diceVarieties)("$diceName",(diceObject) => {
+        let diceResult = rollDice(diceObject.diceSize);
+        expect(diceResult).toBeGreaterThan(0);
+        expect(diceResult).toBeLessThanOrEqual(diceObject.diceSize);
+    })
     
-    test('D6', () => {
-        let result = rollDice();
+    // test('D6', () => {
+    //     let result = rollDice();
 
-        expect(result).toBeGreaterThan(0);
-        expect(result).toBeLessThanOrEqual(6);
-     });
+    //     expect(result).toBeGreaterThan(0);
+    //     expect(result).toBeLessThanOrEqual(6);
+    //  });
 
-    test('D8', () => {
-        let result = rollDice(8);
+    // test('D8', () => {
+    //     let result = rollDice(8);
 
-        expect(result).toBeGreaterThan(0);
-        expect(result).toBeLessThanOrEqual(8);
+    //     expect(result).toBeGreaterThan(0);
+    //     expect(result).toBeLessThanOrEqual(8);
         
-     });
+    //  });
 
-    test('D10', () => {
-        let result = rollDice(10);
+    // test('D10', () => {
+    //     let result = rollDice(10);
 
-        expect(result).toBeGreaterThan(0);
-        expect(result).toBeLessThanOrEqual(10);
+    //     expect(result).toBeGreaterThan(0);
+    //     expect(result).toBeLessThanOrEqual(10);
         
-     });
+    //  });
 
-    test('D12', () => {
-        let result = rollDice(12);
+    // test('D12', () => {
+    //     let result = rollDice(12);
 
-        expect(result).toBeGreaterThan(0);
-        expect(result).toBeLessThanOrEqual(12);
+    //     expect(result).toBeGreaterThan(0);
+    //     expect(result).toBeLessThanOrEqual(12);
         
-     });
+    //  });
 
-    test('D20', () => {
-        let result = rollDice(20);
+    // test('D20', () => {
+    //     let result = rollDice(20);
 
-        expect(result).toBeGreaterThan(0);
-        expect(result).toBeLessThanOrEqual(20);
+    //     expect(result).toBeGreaterThan(0);
+    //     expect(result).toBeLessThanOrEqual(20);
         
-     });
+    //  });
      
 
  });
